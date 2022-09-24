84be54da4cf78fe75284715fe22da73f997ebf83
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-24 19:00:47 +0500

    Update Zadacha_47 and update MyClassLibrary

--- MyClassLibrary/ArrayMy.cs
+++ MyClassLibrary/ArrayMy.cs
@@ -2,11 +2,60 @@ namespace MyClassLibrary;
 
 public class ArrayMy
 {
-    public static void WriteArray<T>(T[] inputArray, string spaceEnd = "")
+    /// Показывает одномерный массив.
+    static public void WriteArray<T>(T[] inputArray, string spaceEnd = "")
     {
         for (int i = 0; i < inputArray.Length; i++)
         {
             Console.Write(inputArray[i] + spaceEnd);
         }
+    }
+
+    /// Показывает двумерный массив.
+    static public void WriteArray2D<T>(T[,] inputArrayND)
+    {
+        int rowsCount = inputArrayND.GetUpperBound(0) + 1;    // Колличество строк
+        int collumsCount = inputArrayND.Length / rowsCount;        // Колличество столбцов
+
+        for (int y = 0; y < rowsCount; y++)
+        {
+            for (int x = 0; x < collumsCount; x++)
+            {
+                Console.Write(inputArrayND[y, x] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    /// Создаёт двумерный массив.
+    static public double[,] Create2DArray(int xCountRows, int yCountCollums)
+    {
+        double[,] array2D = new double[xCountRows, yCountCollums];
+        return array2D;
+    }
+
+    /// Заполняет числами в двумерный массив.
+    static public double[,] Fill2DArrayToNumbers(double[,] inputArray2D, int fromMinNumber, int toMaxNumber, int lengthOfDigitAfterDecimalPoint = 0)
+    {
+        int rowsCount = inputArray2D.GetUpperBound(0) + 1;    // Колличество строк
+        int collumsCount = inputArray2D.Length / rowsCount;        // Колличество столбцов
+
+        for (int xCountRows = 0; xCountRows < rowsCount; xCountRows++)
+        {
+            for (int yCountCollums = 0; yCountCollums < collumsCount; yCountCollums++)
+            {
+                if (lengthOfDigitAfterDecimalPoint != 0)
+                {
+                    inputArray2D[xCountRows, yCountCollums] = Convert.ToDouble(new Random()
+                                                        .Next(fromMinNumber * lengthOfDigitAfterDecimalPoint
+                                                        , toMaxNumber * lengthOfDigitAfterDecimalPoint)) / lengthOfDigitAfterDecimalPoint;
+                }
+                else
+                {
+                    inputArray2D[xCountRows, yCountCollums] = Convert.ToDouble(new Random().Next(fromMinNumber, toMaxNumber));
+                }
+            }
+        }
+        return inputArray2D;
     }
 }
--- Zadacha_47/Program.cs
+++ Zadacha_47/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 
+//  ________________________
+//  | 0,5   7    -2   -0,2 |
+//  |                      |
+//  | 1    -3,3   8   -9,9 |
+//  |                      |
+//  | 8     7,8  -7,1  9   |
+//  ________________________
+
+// m = 3 колличество строк
+// n = 4 колличество рядов
+
+// row      -   ряд      -   горизонталь   -   horizontal
+// height   -   высота   -   вертикаль     -   vercical
+// collum   -   столбец, колонна
+// row      -   ряд
+// string   -   строка.
+// index    -   индекс
+
+// длина  -  length.
+// ширина -  width.
+
+
+// 3D
+// X = horizontal
+// Y = horizontal
+// Z = vercical
+
+// 2D
+// X = horizontal
+// Y = vercical
+
+// Стороны света        Cardinal direction
+// С - Север   Сверху   N - North  Up      = 0°, 360°
+// З - Запад   Слева    E - East   Left    = 90°
+// Ю - Юг      Снизу    S - South  Down    = 180°
+// В - Восток  Справа   W - West   Right   = 270°
+
+using MyClassLibrary;
+
+
+
+int m = 3, n = 4;
+
+double[,] array2DNumbers = ArrayMy.Create2DArray(m, n);
+
+
+
+
+array2DNumbers = ArrayMy.Fill2DArrayToNumbers(array2DNumbers, -100, 100, 2);
+
+ArrayMy.WriteArray2D(array2DNumbers);
