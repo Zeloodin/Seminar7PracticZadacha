36241586013061330f5c5e0f9d69b6a716a45184
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-24 22:08:39 +0500

    very small updated Zadacha_47

--- Zadacha_47/Program.cs
+++ Zadacha_47/Program.cs
@@ -10,35 +10,6 @@
 //  | 8     7,8  -7,1  9   |
 //  ________________________
 
-// m = 3 колличество строк
-// n = 4 колличество рядов
-
-// row      -   ряд      -   горизонталь   -   horizontal
-// height   -   высота   -   вертикаль     -   vercical
-// collum   -   столбец, колонна
-// row      -   ряд
-// string   -   строка.
-// index    -   индекс
-
-// длина  -  length.
-// ширина -  width.
-
-
-// 3D
-// X = horizontal
-// Y = horizontal
-// Z = vercical
-
-// 2D
-// X = horizontal
-// Y = vercical
-
-// Стороны света        Cardinal direction
-// С - Север   Сверху   N - North  Up      = 0°, 360°
-// З - Запад   Слева    E - East   Left    = 90°
-// Ю - Юг      Снизу    S - South  Down    = 180°
-// В - Восток  Справа   W - West   Right   = 270°
-
 using MyClassLibrary;
 
 int m = 3, n = 4;
