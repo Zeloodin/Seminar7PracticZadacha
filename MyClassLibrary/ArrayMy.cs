8e33f8bb614e98d3e372e627ef0130a60ca5a467
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-26 15:58:28 +0500

    Big Updated Zadacha_50 and Big Updated MyClassLibrary

--- MyClassLibrary/ArrayMy.cs
+++ MyClassLibrary/ArrayMy.cs
@@ -53,4 +53,112 @@ public class ArrayMy
         }
         return inputArray2D;
     }
+
+
+
+    /// TODO
+    // string[] SearchInDoubleNestedArray<T>(T[,] inputArray, T inputFindToElement)
+    // {
+    //     string foundElements = string.Empty;
+    //     int rowsCount = inputArray.GetUpperBound(0) + 1;
+    //     for (int rowsI = 0; rowsI < rowsCount; rowsI++)
+    //     {
+    //         if (inputArray[rowsI].GetType() !=)
+    //         {
+    //             foundElements += Array.Find(inputArray[rowsI], element => element == inputFindToElement);
+    //         }
+    //         else
+    //         {
+    //             foundElements +=$"{SearchInArray2D(inputArray[rowsI], inputFindToElement)} ";
+    //         }
+    //     }
+    //     return (foundElements).Split(" ");
+    // }
+
+
+
+    /// TODO
+    /// mode: 0. an element in the array
+    /// mode: 1. shows found items
+    /// mode: 2. shows found items with coordinates
+    /// mode: 3. shows the element by coordinates
+    /// mode: 4. 
+    static public string[,] SearchInArray2D<T>(T[,] inputArray, string elementToFind = "", int mode = 0, int inputRow = 0, int inputCollum = 0)
+    {
+        string foundElements = string.Empty;
+        int rowsCount = inputArray.GetUpperBound(0) + 1;
+        int collumsCount = inputArray.Length / rowsCount;
+        int numberOfFoundElements = 0;
+        if (mode != 3)
+        {
+            for (int rowsI = 0; rowsI < rowsCount; rowsI++)
+            {
+                for (int collumsJ = 0; collumsJ < collumsCount; collumsJ++)
+                {
+                    // Compare specified and selected item
+                    // Сравнить указанный и выбранный элемент 
+                    // input: inputArray, rowsI, collumsJ, elementToFind, mode, numberOfFoundElements
+                    foundElements += SearchInArray2DModes.CompareSpecifiedAndSelectedItem(inputArray: inputArray,
+                                                                                        rows: rowsI,
+                                                                                        collums: collumsJ,
+                                                                                        elementToFind: elementToFind,
+                                                                                        mode: mode,
+                                                                                        numberOfFoundElements: ref numberOfFoundElements);
+
+                }
+            }
+        }
+        if (mode == 0) // Void mode: 0.
+        {
+            string[,] array2DStringEmpty = new string[,] {{""},{""}};
+            if (numberOfFoundElements > 0)
+            {
+                Console.WriteLine($"Элемент для поиска: {elementToFind}, найденно колличество: {numberOfFoundElements}");
+                return array2DStringEmpty;
+            }
+            Console.WriteLine($"Такого элемента {elementToFind} в массиве нет");
+            return array2DStringEmpty;
+        }
+
+        if (mode == 3) // Void mode: 3.
+        {
+            try
+            {
+                string[,] result = new string[,] {{Convert.ToString(inputArray[inputRow, inputCollum])},{""}};
+                return result;
+            }
+            catch(System.IndexOutOfRangeException)
+            {
+                Console.WriteLine("Ошибка. Вышли за рамки границы длины массива.");
+            }
+        }
+
+        string[,] arrayList = new string[rowsCount,3];
+
+        string[] foundElements1DArray = foundElements.Split("[_ss_[_ss_]_ss_]");
+
+        string[] separators = new string[] { "[_ia_[_ia_]_ia_]","[_rc_[_rc_]_rc_]" };
+            
+        for (int i = 0; i < rowsCount; i++)
+        {   
+             for (int j = 0; i < 3; i++)
+            {
+                try
+                {
+                    arrayList[i,j] = (Convert.ToString(foundElements1DArray[i])).Split(separators, StringSplitOptions.RemoveEmptyEntries)[j];
+                }
+                catch
+                {
+                    arrayList[i,j] = "";
+                }
+            }
+        }
+
+        
+
+        return arrayList;
+    }
+
+
+
 }
---
+++ Zadacha_50/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// 
+// Например, задан массив:
+// 
+// 1   4   7   2
+// 
+// 5   9   2   3
+// 
+// 8   4   2   4
+// 
+// 17 -> такого числа в массиве нет
+
+
+// input: 1 2
+// 1 = позиция строки.
+// 2 = позиция ряда.
+// output: 2 из массива, по координатам.
+
+// новый массив ArraySaveFindAll, который хранит координаты значений в указанном массиве 
+// {
+// {"1", 0, 0}, {"4", 0, 1}, {"7", 0, 2}, {"2", 0, 3},
+// {"5", 1, 0}, {"9", 1, 1}, {"2", 1, 2}, {"3", 1, 3},
+// {"8", 2, 0}, {"4", 2, 1}, {"2", 2, 2}, {"4", 2, 3}
+// }
+
+// массив ArraySaveFindAll, отсортированн по возрастанию.
+// {
+// {"1", 0, 0}, {"2", 0, 3}, {"2", 1, 2}, {"2", 2, 2},
+// {"3", 1, 3}, {"4", 0, 1}, {"4", 2, 1}, {"4", 2, 3},
+// {"5", 1, 0}, {"7", 0, 2}, {"8", 2, 0}, {"9", 1, 1}
+// }
+
+using MyClassLibrary;
+
+
+// Для проверки
+// int[,] testArray = new int[,]
+// {
+// {1, 4, 7, 2},
+// {5, 9, 2, 3},
+// {8, 4, 2, 4}
+// };
+
+double[,] testArray = ArrayMy.Fill2DArrayToNumbers(ArrayMy.Create2DArray(25,25), lengthOfDigitAfterDecimalPoint: 0, fromMinNumber: -100, toMaxNumber: 100);
+
+ArrayMy.WriteArray2D(testArray);
+Console.Write("Введите позицию строки: ");
+int rowPosition = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите позицию столбца: ");
+int collumPosition = Convert.ToInt32(Console.ReadLine()) - 1;
+
+Console.Write($"Позиция в массиве [{rowPosition}, {collumPosition}]: ");
+ArrayMy.WriteArray2D(ArrayMy.SearchInArray2D(inputArray: testArray, elementToFind: "", mode: 3, inputRow: rowPosition, inputCollum: collumPosition));
+
+Console.Write("Введите значение для поиска в массиве: ");
+string elementFind = Convert.ToString(Console.ReadLine());
+ArrayMy.WriteArray2D(ArrayMy.SearchInArray2D(inputArray: testArray, elementToFind: elementFind, mode: 0, inputRow: rowPosition, inputCollum: collumPosition));
+
+
+
